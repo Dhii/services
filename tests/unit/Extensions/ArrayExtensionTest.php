7d6501580ea84b239fa2747cb2878e487997e273
Miguel <miguelmuscat93@gmail.com>
2021-06-22 14:30:42 +0200

    Added assoc test for `ArrayExtension`
    This test should fail

--- tests/unit/Extensions/ArrayExtensionTest.php
+++ tests/unit/Extensions/ArrayExtensionTest.php
@@ -67,13 +67,44 @@ class ArrayExtensionTest extends TestCase
     /**
      * @since [*next-version*]
      */
+    public function testInvokeMap()
+    {
+        $prev = [
+            'a' => 'first',
+            'b' => 'second',
+        ];
+
+        $deps = [
+            'c' => 'foo',
+            'd' => 'bar',
+            'e' => 'baz'
+        ];
+
+        $services = [
+            'foo' => 'third',
+            'bar' => 'fourth',
+            'baz' => 'fifth'
+        ];
+
+        $container = MockContainer::with($this, $services);
+
+        $subject = new ArrayExtension($deps);
+        $result = $subject($container, $prev);
+
+        $expected = array_merge($prev, array_combine(array_keys($deps), array_values($services)));
+
+        static::assertEquals($expected, $result);
+    }
+
+    /**
+     * @since [*next-version*]
+     */
     public function testInvokeNoDeps()
     {
         $prev = [
             'first',
             'second',
         ];
-
 
         $container = MockContainer::create($this);
 
